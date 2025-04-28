945219aa6a2963197d69eecc174f728d59248acb
raj-varma-s <rajeshsangani28@gmail.com>
2025-04-28 20:36:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  credentials = file("<YOUR_GCP_CREDENTIALS_JSON_FILE>.json")
+  project     = "<YOUR_PROJECT_ID>"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = "dhbfvffhv"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365
+    }
+  }
+}
